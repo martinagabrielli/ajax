19cb10869277a6e5d4c6332a26288d8e8cd4aef7
martinagabrielli <martina.gabrielli@yahoo.it>
2020-07-27 15:51:44 +0100

    added return to home page links

--- form.php
+++ form.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> </title>
     <link rel="stylesheet" href="form-style.css">
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script> -->
+    <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
+    <a class="home-link" href="/">Home</a>
     <form action="mail.php" method="post"> 
      
     <input  id="mail-name" type="text" name="name" placeholder = "Full name">
--- index.php
+++ index.php
@@ -8,9 +8,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
     <script src="jquery-3.4.1.min.js"></script>
 </head>
 <body>
+    <a class="home-link" href="/">Home</a>
     <div id="comments">
         <?php
             $sql = "SELECT * FROM comments LIMIT 2";
